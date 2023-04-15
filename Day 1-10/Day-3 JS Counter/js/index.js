a30d2dc3a3ad06cf72ee3a2ab7f139f5d843d99d
Harsh <harshpachani@gmail.com>
2023-04-15 21:34:49 +0530

    Day-5

--- Day 1-10/Day-3 JS Counter/js/index.js
+++ Day 1-10/Day-3 JS Counter/js/index.js
@@ -7,14 +7,16 @@
     buttons.forEach(function(button) {
         button.addEventListener('click', function () {
             if (button.classList.contains("decrease-btn")) {
+                console.log("Decrease Clicked");
                 count--;
             } else if (button.classList.contains("increase-btn")) {
+                console.log("Increase Clicked");
                 count++;
             }
 
             const counter = document.querySelector(".counter-machine");
             counter.textContent = count;
-
+            
             if (count > 0) {
                 counter.style.color = "green";
             } else if (count < 0) {
