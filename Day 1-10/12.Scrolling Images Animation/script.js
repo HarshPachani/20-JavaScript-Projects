4b406729b22606ed878ff104f1a1011034e8433d
Harsh <harshpachani@gmail.com>
2023-05-27 17:48:22 +0530

    Scrolling Images Animation

---
+++ Day 1-10/12.Scrolling Images Animation/script.js
@@ -0,0 +1,23 @@
+const card = document.querySelectorAll(".card");
+window.addEventListener("scroll", animateCards);
+
+animateCards();
+
+function animateCards() {
+//   checking the bottom 
+  const checkingBottom = (window.innerHeight / 4) * (125 / 25);
+  card.forEach((singleCard) => {
+    //getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+    const cardTop = singleCard.getBoundingClientRect().top;
+    var toSlideIn = cardTop < checkingBottom;
+    // console.log(toSlideIn);
+//     if we slide below the bottom level this will check
+    if (toSlideIn) {
+//       it will add our new card 
+      singleCard.classList.add("slidingIn");
+    } else {
+//       this will remove the added card
+      singleCard.classList.remove("slidingIn");
+    }
+  });
+}
