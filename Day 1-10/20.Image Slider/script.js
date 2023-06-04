a83032fe3b77f996c431294deadb18a74d76bdcf
Harsh <harshpachani@gmail.com>
2023-06-04 22:42:47 +0530

    Image Slider with JS

---
+++ Day 1-10/20.Image Slider/script.js
@@ -0,0 +1,14 @@
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const slider = document.querySelector(".slider");
+const allImages = document.querySelector(".images");
+
+btn1.addEventListener("click", () => allImages.style.transform = "translateX(-0px)");
+
+btn2.addEventListener("click", () => allImages.style.transform = "translateX(-80%)");
+
+btn3.addEventListener("click", () => allImages.style.transform = "translateX(-170%)");
+
+btn4.addEventListener("click", () => allImages.style.transform = "translateX(-250%)");
